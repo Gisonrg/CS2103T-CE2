f3084b8cb2d7207df3fc9254daad6c4aaff97ff7
Jiang Sheng <gisonrg@gmail.com>
2014-09-20 13:20:39 +0800

    remove test case for CE1 (for trying purpose)

--- src/textBuddyPlus/TextBuddyTest.java
+++ src/textBuddyPlus/TextBuddyTest.java
@@ -10,24 +10,10 @@ public class TextBuddyTest {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		// set up TextBuddy
-//		TextBuddy.main(null);
-		TextBuddy.initializeTextBuddy(null);
+		String [] input = new String[1];
+		input[0] = "input.txt";
 		
-	}
-	
-	@Test
-	public void testGetLineCount() {
-		TextBuddy.executeCommand("clear");
-		assertEquals(0, TextBuddy.getLineCount());
-		
-		TextBuddy.executeCommand("add Task1");
-		assertEquals(1, TextBuddy.getLineCount());
-		
-		TextBuddy.executeCommand("add Task2");
-		assertEquals(2, TextBuddy.getLineCount());
-		
-		TextBuddy.executeCommand("clear");
-		assertEquals(0, TextBuddy.getLineCount());
+		TextBuddy.initializeTextBuddy(input);
 	}
 	
 	@Test
